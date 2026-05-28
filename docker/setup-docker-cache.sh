5f3a7f17338c08d83d7da5593e91917fea3e5dd3
willyhorizont <willyhorizont@gmail.com>
2026-05-28 21:27:22 +0700

    update setup-docker-cache.sh

--- docker/setup-docker-cache.sh
+++ docker/setup-docker-cache.sh
@@ -2,6 +2,7 @@
 
 clear_docker_cache() {
     sudo docker rm -f local-cache
+    sudo rm -rf /var/lib/docker-cache
 }
 
 print_docker_cache(){
@@ -15,7 +16,7 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 sudo docker run -d -p 127.0.0.1:5000:5000 \
   --name local-cache \
-  -e REGISTRY_PROXY_REMOTEURL=https://docker.io \
+  -e REGISTRY_PROXY_REMOTEURL=https://registry-1.docker.io \
   -v /var/lib/docker-cache:/var/lib/registry \
   --restart always \
   registry:2
