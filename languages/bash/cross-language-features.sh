b84438da730ee8f13015335a078c3c0c99afc8b1
willyhorizont <willyhorizont@gmail.com>
2026-06-06 14:23:18 +0700

    small fix in bash

--- languages/bash/cross-language-features.sh
+++ languages/bash/cross-language-features.sh
@@ -2,7 +2,7 @@
 
 (
     # 1. support function as value
-    declare -r say_hello=$((echo '#!/bin/bash
+    declare -r say_hello=$( (echo '#!/bin/bash
 
                                 declare -r -a variadic_arguments=( "$@" )
                                 declare -r callback_function="$1"
@@ -11,14 +11,14 @@
                                 bash -c "$callback_function" _ "${variadic_arguments[@]:1}"
                                 ') | sed 's/^[ \t]*//' | jq -Rs . | jq -r .
                             )
-    bash -c "$say_hello" _ "$((echo '#!/bin/bash
+    bash -c "$say_hello" _ "$( (echo '#!/bin/bash
 
                                     declare -r -a variadic_arguments=( "$@" )
 
                                     echo "world"
                                     ') | sed 's/^[ \t]*//' | jq -Rs . | jq -r .
                                 )"
-    declare -r multiply=$((echo '#!/bin/bash
+    declare -r multiply=$( (echo '#!/bin/bash
 
                                 declare -r -a variadic_arguments=( "$@" )
                                 declare -r a="$1"
@@ -50,7 +50,7 @@
                                                 -123.789,
                                                 [1, 2, 3],
                                                 { \"foo\": \"bar\" },
-                                                $((echo '#!/bin/bash
+                                                $( (echo '#!/bin/bash
 
                                                         declare -r -a variadic_arguments=( "$@" )
                                                         declare -r a="$1"
@@ -73,7 +73,7 @@
                                                 \"some_float_negative\": -123.789,
                                                 \"some_python_like_list\": [1, 2, 3],
                                                 \"some_python_like_dict\": { \"foo\": \"bar\" },
-                                                \"some_function\": $((echo '#!/bin/bash
+                                                \"some_function\": $( (echo '#!/bin/bash
 
                                                                             declare -r -a variadic_arguments=( "$@" )
                                                                             declare -r a="$1"
