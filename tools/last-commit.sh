cbbb881a9873aa4255cc7e4f8ff5aca7097ec77f
willyhorizont <willyhorizont@gmail.com>
2026-07-17 23:54:15 +0700

    [Last updated: 17 Jul 2026 @ 11:54 PM] version 2.7.53: re re commit finish c-sharp runtime init; small update in visual-basic-dot-net runtime init; working progress add escape-string and std-json-stringify in runtime/*

--- tools/last-commit.sh
+++ tools/last-commit.sh
@@ -2,7 +2,7 @@
 
 SD=$(dirname "$(realpath "$0")")
 RD=$(realpath "$SD/..")
-V="53" # ! DON'T FORGET TO CHANGE VERSION BEFORE RUNNING !!!!
+V="2.7.53" # ! DON'T FORGET TO CHANGE VERSION BEFORE RUNNING !!!!
 T=$(date "+%d %b %Y @ %I:%M %p")
 \. "$HOME/.nvm/nvm.sh"
 npm version "$V" --no-git-tag-version
@@ -14,7 +14,7 @@ version $V:
 H=$(sed -e '/./,$!d' <<< "$H")
 # ! DON'T FORGET TO CHANGE COMMIT MESSAGE BEFORE RUNNING !!!!
 M="
-re commit
+re re commit
 finish c-sharp runtime init;
 small update in visual-basic-dot-net runtime init;
 working progress add escape-string and std-json-stringify in runtime/*
