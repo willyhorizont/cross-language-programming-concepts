687ff4a26c0b1c1581d5b56e1d424f9245ec5401
willyhorizont <willyhorizont@gmail.com>
2026-07-20 12:57:28 +0700

    [Last updated: 20 Jul 2026 @ 12:57 PM] version 2.7.62: small update in cangjie runtime init;

--- languages/erlang/hello-world.erl
+++ languages/erlang/hello-world.erl
@@ -3,3 +3,5 @@
 
 start() ->
     io:format("hello, world~n").
+
+    ok.
--- tools/last-commit.sh
+++ tools/last-commit.sh
@@ -2,7 +2,7 @@
 
 SD=$(dirname "$(realpath "$0")")
 RD=$(realpath "$SD/..")
-V="2.7.61" # ! DON'T FORGET TO CHANGE VERSION BEFORE RUNNING !!!!
+V="2.7.62" # ! DON'T FORGET TO CHANGE VERSION BEFORE RUNNING !!!!
 T=$(date "+%d %b %Y @ %I:%M %p")
 cd "$RD" || exit
 \. "$HOME/.nvm/nvm.sh"
@@ -14,7 +14,7 @@ version $V:
 H=$(sed -e '/./,$!d' <<< "$H")
 # ! DON'T FORGET TO CHANGE COMMIT MESSAGE BEFORE RUNNING !!!!
 M="
-finish cangjie runtime init;
+small update in cangjie runtime init;
 "
 M=$(sed -e '/./,$!d' <<< "$M")
 M="$H
