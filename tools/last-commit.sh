8148e169571d0e1911472693bb5b34a9e59d4aa3
willyhorizont <willyhorizont@gmail.com>
2026-08-03 04:06:03 +0700

    [Last updated: 03 Aug 2026 @ 04:06 AM] version 2.7.89: update wren Dockerfile;

--- tools/last-commit.sh
+++ tools/last-commit.sh
@@ -2,7 +2,7 @@
 
 SD=$(dirname "$(realpath "$0")")
 RD=$(realpath "$SD/..")
-V="2.7.88" # ! DON'T FORGET TO CHANGE VERSION BEFORE RUNNING !!!!
+V="2.7.89" # ! DON'T FORGET TO CHANGE VERSION BEFORE RUNNING !!!!
 T=$(date "+%d %b %Y @ %I:%M %p")
 cd "$RD" || exit
 \. "$HOME/.nvm/nvm.sh"
@@ -14,14 +14,17 @@ version $V:
 H=$(sed -e '/./,$!d' <<< "$H")
 # ! DON'T FORGET TO CHANGE COMMIT MESSAGE BEFORE RUNNING !!!!
 M="
-update wren runtime init;
+update wren Dockerfile;
 "
 M=$(sed -e '/./,$!d' <<< "$M")
 M="$H
 $M"
 awk -v msg="$M" 'BEGIN {print msg; print ""} {print}' "$RD/changelog.txt" > "$RD/changelog.tmp" && mv "$RD/changelog.tmp" "$RD/changelog.txt"
-"$RD/languages/python/runner.sh" "$RD/tools/generate-readme.py"
-git add .
+git add changelog.txt
+git add package-lock.json
+git add package.json
+# "$RD/languages/python/runner.sh" "$RD/tools/generate-readme.py"
+# git add .
 git commit -m "$M"
 git tag -d "$V" 2>/dev/null
 git tag -a "$V" -m "$M"
