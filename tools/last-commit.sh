7b1a58b1444c2cac0ddbe09dd215ed1d1373ce6f
willyhorizont <willyhorizont@gmail.com>
2026-08-05 11:31:21 +0700

    [Last updated: 05 Aug 2026 @ 11:31 AM] version 2.7.94: cancel learn beef, the language still work in progess and under active development; small update in c-sharp; small fix in objective-c runner.sh; update notes;

--- tools/last-commit.sh
+++ tools/last-commit.sh
@@ -2,7 +2,7 @@
 
 SD=$(dirname "$(realpath "$0")")
 RD=$(realpath "$SD/..")
-V="2.7.93" # ! DON'T FORGET TO CHANGE VERSION BEFORE RUNNING !!!!
+V="2.7.94" # ! DON'T FORGET TO CHANGE VERSION BEFORE RUNNING !!!!
 T=$(date "+%d %b %Y @ %I:%M %p")
 cd "$RD" || exit
 \. "$HOME/.nvm/nvm.sh"
@@ -17,6 +17,7 @@ M="
 cancel learn beef, the language still work in progess and under active development;
 small update in c-sharp;
 small fix in objective-c runner.sh;
+update notes;
 "
 M=$(sed -e '/./,$!d' <<< "$M")
 M="$H
