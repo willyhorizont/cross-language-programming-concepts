3ec459c8f5790abe89fedc2c2eb5f9939c5e8d63
willyhorizont <willyhorizont@gmail.com>
2026-08-17 16:24:24 +0700

    [Last updated: 17 Aug 2026 @ 04:24 PM][version: 2.8.1] chmod +x runner.sh;

--- tools/last-commit.sh
+++ tools/last-commit.sh
@@ -2,7 +2,7 @@
 
 SD=$(dirname "$(realpath "$0")")
 RD=$(realpath "$SD/..")
-V="2.8.0" # ! DON'T FORGET TO CHANGE VERSION BEFORE RUNNING !!!!
+V="2.8.1" # ! DON'T FORGET TO CHANGE VERSION BEFORE RUNNING !!!!
 T=$(date "+%d %b %Y @ %I:%M %p")
 cd "$RD" || exit
 
@@ -24,20 +24,7 @@ H="
 H=$(sed -e '/./,$!d' <<< "$H")
 # ! DON'T FORGET TO CHANGE COMMIT MESSAGE BEFORE RUNNING !!!!
 M="
-update last-commit.sh;
-small update in pike;
-small update in c;
-small update in odin;
-small update in c-sharp;
-small update in cangjie;
-small update in go;
-small update in gdscript;
-small update in v;
-update in d;
-update in dart;
-update in vb;
-update Dockerfile;
-update runner.sh;
+chmod +x runner.sh;
 "
 M=$(sed -e '/./,$!d' <<< "$M")
 M="$H
