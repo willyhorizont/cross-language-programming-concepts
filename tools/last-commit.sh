7bdcc6301e9fa47cc0e2aed147c8d1891bbf7ad5
Willy Horizont <willyhorizont@gmail.com>
2026-08-20 01:29:56 +0700

    [Last updated: 20 Aug 2026 @ 01:29 AM][version: 2.9.2] update github-programming-languages-card.png;

--- tools/last-commit.sh
+++ tools/last-commit.sh
@@ -2,7 +2,7 @@
 
 SD=$(dirname "$(realpath "$0")")
 RD=$(realpath "$SD/..")
-V="2.9.1" # ! DON'T FORGET TO CHANGE VERSION BEFORE RUNNING !!!!
+V="2.9.2" # ! DON'T FORGET TO CHANGE VERSION BEFORE RUNNING !!!!
 T=$(date "+%d %b %Y @ %I:%M %p")
 cd "$RD" || exit
 
@@ -24,16 +24,7 @@ H="
 H=$(sed -e '/./,$!d' <<< "$H")
 # ! DON'T FORGET TO CHANGE COMMIT MESSAGE BEFORE RUNNING !!!!
 M="
-update zig modes;
-update smalltalk modes;
-small update in c3;
-small update in d;
-small update in java;
-small update in odin;
-update in powershell;
-update in rust;
-update in scala;
-update new-notes.txt;
+update github-programming-languages-card.png;
 "
 M=$(sed -e '/./,$!d' <<< "$M")
 M="$H
