9fcb7d78a220057bc43205f17e06d19dcd11b8d1
willyhorizont <willyhorizont@gmail.com>
2026-08-21 02:41:00 +0700

    [Last updated: 21 Aug 2026 @ 02:40 AM][version: 2.9.8] update github-programming-languages-card.png; update languages.json colors;

--- tools/last-commit.sh
+++ tools/last-commit.sh
@@ -2,7 +2,7 @@
 
 SD=$(dirname "$(realpath "$0")")
 RD=$(realpath "$SD/..")
-V="2.9.7" # ! DON'T FORGET TO CHANGE VERSION BEFORE RUNNING !!!!
+V="2.9.8" # ! DON'T FORGET TO CHANGE VERSION BEFORE RUNNING !!!!
 T=$(date "+%d %b %Y @ %I:%M %p")
 cd "$RD" || exit
 
@@ -24,7 +24,8 @@ H="
 H=$(sed -e '/./,$!d' <<< "$H")
 # ! DON'T FORGET TO CHANGE COMMIT MESSAGE BEFORE RUNNING !!!!
 M="
-update .vscode/settings.json;
+update github-programming-languages-card.png;
+update languages.json colors;
 "
 M=$(sed -e '/./,$!d' <<< "$M")
 M="$H
