786ae65560e64f9d82415fed5f1ddfcd90dccb4e
willyhorizont <willyhorizont@gmail.com>
2026-08-22 08:40:55 +0700

    [Last updated: 22 Aug 2026 @ 08:40 AM][version: 2.9.9] add .kateconfig; update code-runner config;

--- tools/last-commit.sh
+++ tools/last-commit.sh
@@ -2,7 +2,7 @@
 
 SD=$(dirname "$(realpath "$0")")
 RD=$(realpath "$SD/..")
-V="2.9.8" # ! DON'T FORGET TO CHANGE VERSION BEFORE RUNNING !!!!
+V="2.9.9" # ! DON'T FORGET TO CHANGE VERSION BEFORE RUNNING !!!!
 T=$(date "+%d %b %Y @ %I:%M %p")
 cd "$RD" || exit
 
@@ -24,8 +24,8 @@ H="
 H=$(sed -e '/./,$!d' <<< "$H")
 # ! DON'T FORGET TO CHANGE COMMIT MESSAGE BEFORE RUNNING !!!!
 M="
-update github-programming-languages-card.png;
-update languages.json colors;
+add .kateconfig;
+update code-runner config;
 "
 M=$(sed -e '/./,$!d' <<< "$M")
 M="$H
